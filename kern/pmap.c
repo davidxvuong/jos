57fc21924c934100151803af319e2be933eb7f22
David Vuong <david.vuong@uwaterloo.ca>
2024-12-20 16:54:25 -0800

    Map the rest of the virtual memory space

--- kern/pmap.c
+++ kern/pmap.c
@@ -184,7 +184,7 @@ mem_init(void)
 	//    - the new image at UPAGES -- kernel R, user R
 	//      (ie. perm = PTE_U | PTE_P)
 	//    - pages itself -- kernel RW, user NONE
-	// Your code goes here:
+	boot_map_region(kern_pgdir, UPAGES, PTSIZE, PADDR(pages), PTE_U);
 
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
@@ -196,7 +196,8 @@ mem_init(void)
 	//       the kernel overflows its stack, it will fault rather than
 	//       overwrite memory.  Known as a "guard page".
 	//     Permissions: kernel RW, user NONE
-	// Your code goes here:
+	uintptr_t kernel_stack_addr = KSTACKTOP-KSTKSIZE;
+	boot_map_region(kern_pgdir, kernel_stack_addr, KSTKSIZE, PADDR(bootstack), PTE_W);
 
 	//////////////////////////////////////////////////////////////////////
 	// Map all of physical memory at KERNBASE.
@@ -206,6 +207,8 @@ mem_init(void)
 	// we just set up the mapping anyway.
 	// Permissions: kernel RW, user NONE
 	// Your code goes here:
+	uintptr_t pa_end_size = 0xffffffff - KERNBASE + 1;
+	boot_map_region(kern_pgdir, KERNBASE, pa_end_size, 0, PTE_W);
 
 	// Check that the initial page directory has been set up correctly.
 	check_kern_pgdir();
