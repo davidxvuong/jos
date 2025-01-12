1ea24c8cc0db4a033a9ff7a48f042eacc7e887c8
David Vuong <david.vuong@uwaterloo.ca>
2025-01-12 01:58:52 -0800

    Lab5 Ex3

--- fs/fs.c
+++ fs/fs.c
@@ -64,8 +64,17 @@ alloc_block(void)
 	// contains the in-use bits for BLKBITSIZE blocks.  There are
 	// super->s_nblocks blocks in the disk altogether.
 
-	// LAB 5: Your code here.
-	panic("alloc_block not implemented");
+	uint32_t i = 0;
+
+	for (i = 1; i < super->s_nblocks; i++)
+	{
+		if (block_is_free(i))
+		{
+			bitmap[i/32] ^= 1 << (i%32);
+			flush_block(diskaddr(i));
+			return i;
+		}
+	}
 	return -E_NO_DISK;
 }
 
