652029d6a22f1fa63516392812ebded77735ab0b
David Vuong <david.vuong@uwaterloo.ca>
2025-01-18 23:20:23 -0800

    Lab6 Ex4

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,9 +1,17 @@
 #include <kern/e1000.h>
 #include <kern/pmap.h>
 
+volatile uint32_t *e1000_base = NULL;
+
+// Macros
+#define E1000_REG(offset) (e1000_base[offset / 4])
+
 int e1000_attach(struct pci_func *pcif)
 {
     pci_func_enable(pcif);
+
+    e1000_base = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
+    assert(E1000_REG(E1000_STATUS) == 0x80080783);
 
     return 0;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -3,10 +3,15 @@
 
 #include <kern/pci.h>
 
+// E1000 Device ID's
 #define E1000_VEND_ID                       0x8086
 #define E1000_DESKTOP_DEV_ID                0x100E
 #define E1000_MOBILE_DEV_ID                 0x1015
 
+// E1000 Registers
+#define E1000_STATUS                        0x00008  /* Device Status - RO */
+
+// Function definitions
 int e1000_attach(struct pci_func *pcif);
 
 #endif  // SOL >= 6
