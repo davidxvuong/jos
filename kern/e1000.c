153e2c17325f2633e0c5c13c41d205fa945ae4bb
David Vuong <david.vuong@uwaterloo.ca>
2025-01-18 22:37:55 -0800

    Lab6 Ex3

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,4 +1,9 @@
 #include <kern/e1000.h>
 #include <kern/pmap.h>
 
-// LAB 6: Your driver code here
+int e1000_attach(struct pci_func *pcif)
+{
+    pci_func_enable(pcif);
+
+    return 0;
+}
--- kern/e1000.h
+++ kern/e1000.h
@@ -1,3 +1,12 @@
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
+
+#include <kern/pci.h>
+
+#define E1000_VEND_ID                       0x8086
+#define E1000_DESKTOP_DEV_ID                0x100E
+#define E1000_MOBILE_DEV_ID                 0x1015
+
+int e1000_attach(struct pci_func *pcif);
+
 #endif  // SOL >= 6
