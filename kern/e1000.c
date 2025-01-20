20316acb16dd54c9bda5206659a62a1f7b3a9183
David Vuong <david.vuong@uwaterloo.ca>
2025-01-19 20:07:49 -0800

    Lab6 Ex5

--- kern/e1000.c
+++ kern/e1000.c
@@ -6,12 +6,61 @@ volatile uint32_t *e1000_base = NULL;
 // Macros
 #define E1000_REG(offset) (e1000_base[offset / 4])
 
+// Allocate a region of memory for the transmit descriptor list. 
+// Software should insure this memory is aligned on a paragraph (16-byte) boundary.
+struct e1000_tx_desc tx_array[E1000_TX_DESC_COUNT] = {0};
+char tx_buf[E1000_TX_DESC_COUNT * E1000_PACKET_SIZE_BYTES] = {0};
+
+// Function Definitions
+
 int e1000_attach(struct pci_func *pcif)
 {
+    int i = 0;
+
     pci_func_enable(pcif);
 
     e1000_base = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
     assert(E1000_REG(E1000_STATUS) == 0x80080783);
+
+    // Transmit Initialization
+
+    // Reserve memory for the transmit descriptor array and the packet buffers pointed to by the transmit descriptors.
+    for (i = 0; i < E1000_TX_DESC_COUNT; i++)
+    {
+        tx_array[i].addr = PADDR(tx_buf + (E1000_TX_DESC_COUNT * E1000_PACKET_SIZE_BYTES));
+        tx_array[i].length = E1000_PACKET_SIZE_BYTES;
+        tx_array[i].status |= E1000_TXD_STAT_DD & 1;
+        tx_array[i].cmd |= (E1000_TXD_CMD_EOP & 1) | (E1000_TXD_CMD_RS & (1 << 3));
+    }
+
+    // Program the Transmit Descriptor Base Address (TDBAL/TDBAH) register(s) with the address of the
+    // region. TDBAL is used for 32-bit addresses and both TDBAL and TDBAH are used for 64-bit addresses.
+    E1000_REG(E1000_TDBAL) = PADDR(tx_array);
+    E1000_REG(E1000_TDBAH) = 0;
+
+    // Set the Transmit Descriptor Length (TDLEN) register to the size (in bytes) of the descriptor ring.
+    // This register must be 128-byte aligned.
+    E1000_REG(E1000_TDLEN) = E1000_TX_DESC_COUNT * E1000_PACKET_SIZE_BYTES;
+
+    // The Transmit Descriptor Head and Tail (TDH/TDT) registers are initialized (by hardware) to 0b
+    // after a power-on or a software initiated Ethernet controller reset. Software should write 0b to both
+    // these registers to ensure this.
+    E1000_REG(E1000_TDH) = 0;
+    E1000_REG(E1000_TDT) = 0;
+
+    // Set the Enable (TCTL.EN) bit to 1b for normal operation.
+    E1000_REG(E1000_TCTL) |= E1000_TCTL_EN & 1;
+
+    // Set the Pad Short Packets (TCTL.PSP) bit to 1b.
+    E1000_REG(E1000_TCTL) |= E1000_TCTL_PSP & (1 << 3);
+
+    // Configure the Collision Distance (TCTL.COLD) to its expected value. For full duplex
+    // operation, this value should be set to 40h. For gigabit half duplex, this value should be set to
+    // 200h. For 10/100 half duplex, this value should be set to 40h.
+    E1000_REG(E1000_TCTL) |= E1000_TCTL_COLD & (0x40 << 12);
+
+    // Set the Inter Packet Gap to the default values as listed in section 13.4.34
+    E1000_REG(E1000_TIPG) = 0xA | (0x4 << 10) | (0x6 << 20);
 
     return 0;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -9,7 +9,62 @@
 #define E1000_MOBILE_DEV_ID                 0x1015
 
 // E1000 Registers
+// This is taken from https://pdos.csail.mit.edu/6.828/2018/labs/lab6/e1000_hw.h as needed
 #define E1000_STATUS                        0x00008  /* Device Status - RO */
+#define E1000_TDBAL                         0x03800  /* TX Descriptor Base Address Low - RW */
+#define E1000_TDBAH                         0x03804  /* TX Descriptor Base Address High - RW */
+#define E1000_TDLEN                         0x03808  /* TX Descriptor Length - RW */
+#define E1000_TDH                           0x03810  /* TX Descriptor Head - RW */
+#define E1000_TDT                           0x03818  /* TX Descripotr Tail - RW */
+#define E1000_TCTL                          0x00400  /* TX Control - RW */
+#define E1000_TCTL_EXT                      0x00404  /* Extended TX Control - RW */
+#define E1000_TIPG                          0x00410  /* TX Inter-packet gap -RW */
+
+// TX Descriptor bit definitions
+#define E1000_TXD_CMD_EOP                   0x01000000 /* End of Packet */
+#define E1000_TXD_CMD_RS                    0x08000000 /* Report Status */
+#define E1000_TXD_STAT_DD                   0x00000001 /* Descriptor Done */
+
+// TX Control
+#define E1000_TCTL_EN                       0x00000002    /* enable tx */
+#define E1000_TCTL_PSP                      0x00000008    /* pad short packets */
+#define E1000_TCTL_CT                       0x00000ff0    /* collision threshold */
+#define E1000_TCTL_COLD                     0x003ff000    /* collision distance */
+
+// E1000/JOS Definitions
+#define E1000_TX_DESC_COUNT                 8
+#define E1000_PACKET_SIZE_BYTES             1518
+
+// Transmit Descriptor
+
+// Transmit Descriptor Fields
+// Each transmit descriptor is 16 bytes long and contains the following fields:
+
+// Buffer Address (64 bits):
+// A pointer to the location in memory where the packet data resides.
+// Length (16 bits):
+// Specifies the length of the data in the buffer (in bytes).
+// Checksum Offset (CSO, 8 bits):
+// Indicates where the card should begin checksum calculations within the buffer.
+// Command (8 bits):
+// Defines actions the card should take for this descriptor, such as initiating transmission or enabling interrupts.
+// Status (8 bits):
+// Provides feedback from the controller on the processing state of the descriptor.
+// Checksum Start Field (CSS, 8 bits):
+// Specifies the starting byte for checksum calculations.
+// Special (16 bits):
+// Reserved for special purposes, often application-specific.
+struct e1000_tx_desc
+{
+	uint64_t addr;
+	uint16_t length;
+	uint8_t cso;
+	uint8_t cmd;
+	uint8_t status;
+	uint8_t css;
+	uint16_t special;
+} __attribute__((packed));;
+
 
 // Function definitions
 int e1000_attach(struct pci_func *pcif);
